df40adcacabc0a403fac62b5f0e5c3bc86a3d702
unknown <mala_cara1@msn.com>
2019-11-21 13:49:59 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         UPDATE React IIIII
+        </a>
+      </header>
+      <p className="App-intro">
+        Update  con REACT JS IIII
+      </p>
+      <p>otro Texto para prueba</p>
+    </div>
+    
+  );
+}
+
+export default App;
